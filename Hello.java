4e4e7d33c9cb8d448bf748ebcb19e65b100a68be
unknown <dm21cris@S50956.siena.edu>
2015-01-26 18:59:28 -0500

    fixed stupid characters

--- Hello.java
+++ Hello.java
@@ -3,10 +3,7 @@ public class Hello
   public static void main(String args[])
   {
     System.out.println("Hello, my name is Amar");
-<<<<<<< HEAD
 	System.out.println("Josh was here.");
-=======
 	System.out.println("Hello, my name is Damian);
->>>>>>> d207595641f15fff5a1b27c92fdc16bc297df18c
   }
 }
